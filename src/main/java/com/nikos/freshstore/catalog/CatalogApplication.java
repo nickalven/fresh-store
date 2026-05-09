8fe61b74cb62ffbf393a0a2c426abe6613424666
Nikos Alveniotis <nickalven@hotmail.com>
2026-05-09 11:02:17 +0300

    changed packaging to war

--- src/main/java/com/nikos/freshstore/catalog/CatalogApplication.java
+++ src/main/java/com/nikos/freshstore/catalog/CatalogApplication.java
@@ -2,12 +2,18 @@ package com.nikos.freshstore.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatalogApplication {
+public class CatalogApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CatalogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
-
 }
